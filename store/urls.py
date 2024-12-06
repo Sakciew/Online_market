a2647e56aa6d4c692154ace980656425ec23031b
Sakciew <shijaganiyev@gmail.com>
2024-12-06 11:46:13 +0500

    Ilk commit

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-
-from store.views import home
+from store.views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('', home, name = 'name')
-]
+    path('', home, name = 'name'),
+    path('contact', contact, name = 'contact'),
+    path('products/<slug>/', products, name = 'products'),
+    path('register', register, name = 'register'),
+    path('single', single, name = 'single'),
+
+
+
+
+]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
